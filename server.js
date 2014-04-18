a69be7d5c0f9b502f94e01ee98662f22d21d2ee7
stefanKuijers <stefan@kuijers.nl>
2014-04-18 13:58:28 +0200

    added some stuff to the basic server

--- server.js
+++ server.js
@@ -3,38 +3,43 @@
 var restify = require('restify');
 
 var server = restify.createServer({
-  name: 'project-api'
+   name: 'project-api'
 });
 
 server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
-server.get('/get', get_from);         // curl -i http://localhost:8888/get
-server.post('/create', insert_into); 
-server.put('/update', update_at);    
-server.del('/delete', delete_from);  
+server.get('/get', get_from);          // curl -i http://localhost:8888/get
+server.post('/create', insert_into);
+server.put('/update', update_at);
+server.del('/delete', delete_from);
 
 server.listen(8888, function() {
-  console.log('%s listening at %s', server.name, server.url);
+
+   console.log('%s listening at %s', server.name, server.url);
 });
 
-function callback (err, result) {
-  return err ? res.send(err) : res.json(result);
+function callback(err, result) {
+   return err ? res.send(err) : res.json(result);
 }
 
 function get_from(req, res) {
-  console.log("get_from");
+   res.send("response after get_from");
+   console.log("get_from");
 }
 
 function insert_into(req, res) {
-  console.log("insert_into");
+   res.send("response after insert_into");
+   console.log("insert_into");
 }
 
 function update_at(req, res) {
+   res.send("response after update_at");
    console.log("update_at");
 }
 
 function delete_from(req, res) {
+   res.send("response after delete_from");
    console.log("delete_from");
 }
 
